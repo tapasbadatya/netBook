9ba5dc026e7d1649310490e6a80640f1f959767e
tapasbadatya <tapas.kumarbadatya@gmail.com>
2018-07-06 18:46:32 +0530

    commit

--- web/scripts/MyDirectory.js
+++ web/scripts/MyDirectory.js
@@ -1,3 +1,31 @@
 /**
  * Created by admin on 06-07-2018.
  */
+var Directory = new function(){
+
+
+
+    var renamedName="";
+
+    this.applyRename = function(id){
+
+
+
+        var name = document.getElementById(id).innerText;
+        document.getElementById(id).innerHTML="<input type='text' value='"+name+"' id='editFolderName' onblur=\"Directory.rename('"+id+"','"+name+"')\" onkeyup=\"Directory.getRenamedVal('editFolderName')\">";
+        renamedName = name;
+        document.getElementById(id).removeAttribute("onclick");
+    };
+
+    this.getRenamedVal = function(id){
+      renamedName = document.getElementById(id).value;
+    };
+    this.rename = function(id,name){
+        document.getElementById(id).innerHTML="";
+        document.getElementById(id).innerText=renamedName;
+        document.getElementById(id).setAttribute("onclick","Directory.applyRename('"+id+"')");
+
+    };
+
+
+};
