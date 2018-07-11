ec957f258335b9fdad7c82d4308c9b652dc4266c
tapasbadatya <tapas.kumarbadatya@gmail.com>
2018-07-11 16:05:55 +0530

    commit

--- web/scripts/MyDirectory.js
+++ web/scripts/MyDirectory.js
@@ -6,12 +6,11 @@ var Directory = new function(){
 
 
     var renamedName="";
+    var name="";
 
     this.applyRename = function(id){
 
-
-
-        var name = document.getElementById(id).innerText;
+        name = document.getElementById(id).innerText;
         document.getElementById(id).innerHTML="<input type='text' value='"+name+"' id='editFolderName' onblur=\"Directory.rename('"+id+"','"+name+"')\" onkeyup=\"Directory.getRenamedVal('editFolderName')\">";
         renamedName = name;
         document.getElementById(id).removeAttribute("onclick");
@@ -21,10 +20,15 @@ var Directory = new function(){
       renamedName = document.getElementById(id).value;
     };
     this.rename = function(id,name){
-        document.getElementById(id).innerHTML="";
-        document.getElementById(id).innerText=renamedName;
+        if(renamedName.length<=0){
+            document.getElementById(id).innerHTML="";
+            document.getElementById(id).innerText=name;
+        }
+        else{
+            document.getElementById(id).innerHTML="";
+            document.getElementById(id).innerText=renamedName;
+        }
         document.getElementById(id).setAttribute("onclick","Directory.applyRename('"+id+"')");
-
     };
 
 
